055f64a65c4fcb8c593b8d4ccfaf6411a1c0cbcd
Alicja Chruszczyk <alicja.chruszczyk@gmail.com>
2018-03-04 20:52:35 +0100

    Change scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+// 9.2
+
+var femaleNames = ['Ala', 'Aga', 'Klaudia', 'Marta', 'Klara'];
+var maleNames = ['Michał', 'Łukasz', 'Mateusz', 'Adam', 'Piotr'];
+
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames);
+
+var newName = 'Tomek';
+if (allNames.indexOf(newName) === -1) {
+	allNames.push(newName);
+}
+console.log(allNames);
+
+
+/* 9.1
 
 function getTriangleArea(a, h) {
 	if (a > 0 && h > 0) {
@@ -13,3 +29,4 @@ console.log(getTriangleArea(10, 6));
 var triangle1Area = getTriangleArea(3, 4);
 var triangle2Area = getTriangleArea(8, 17);
 var triangle3Area = getTriangleArea(10, 7);
+*/
